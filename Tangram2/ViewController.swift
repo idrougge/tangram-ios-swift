4555bea055915f4eb5c4771a8fba4105752ed926
Iggy Drougge <idrougge@mac.com>
2016-04-08 14:29:15 +0200

    Initial Commit

---
+++ Tangram2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tangram2
+//
+//  Created by Iggy Drougge on 2016-04-08.
+//  Copyright © 2016 Iggy Drougge. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
